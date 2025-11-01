a8dcef8bfd02f842a3dbd8a76914f275a51a9ae8
Esder <cortesf.joseantonio@gmail.com>
2025-11-01 13:09:39 +0100

    docker env and normalize avatarPath

--- lib/helpers/utils.php
+++ lib/helpers/utils.php
@@ -16,4 +16,25 @@ function getRoles() {
 function getID() {
     return getNextId();
 }
+
+/**
+ * Normaliza la ruta del avatar para que funcione tanto en Docker como en XAMPP
+ * Elimina /CRUDle/ del path si existe y añade el WEB_ROOT correcto
+ */
+function normalizeAvatarPath($avatarPath) {
+    if (empty($avatarPath)) {
+        return null;
+    }
+    
+    // Si la ruta ya tiene /CRUDle/, la quitamos
+    $cleanPath = str_replace('/CRUDle/', '/', $avatarPath);
+    
+    // Aseguramos que empiece con /
+    if (substr($cleanPath, 0, 1) !== '/') {
+        $cleanPath = '/' . $cleanPath;
+    }
+    
+    // Añadimos el WEB_ROOT correcto según el entorno
+    return WEB_ROOT . $cleanPath;
+}
 ?>
