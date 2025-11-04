cd77dcd159229a87e681d3d76170a3ee58905470
Esder <cortesf.joseantonio@gmail.com>
2025-11-04 22:05:21 +0100

    added youtube link

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -8,6 +8,8 @@
                 <a href="https://github.com/EsderJ10" target="_blank" rel="noopener noreferrer" title="View creator's GitHub">
                     <i class="fab fa-github"></i>
                 </a>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer" title="View creator's YouTube channel">
+                    <i class="fab fa-youtube"></i>
             </div>
         </footer>
     </div>
