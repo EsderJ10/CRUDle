7583a71b0dedc51b42da387b80d9428274721af3
esder <cortesf.joseantonio@gmail.com>
2025-11-26 22:49:37 +0100

    removed redundant

--- pages/users/user_info.php
+++ pages/users/user_info.php
@@ -6,11 +6,8 @@
  */
 
 require_once '../../config/init.php';
-require_once getPath('config/config.php');
 require_once getPath('lib/business/user_operations.php');
 require_once getPath('lib/presentation/user_views.php');
-require_once getPath('lib/core/error_handler.php');
-require_once getPath('lib/core/exceptions.php');
 
 $pageTitle = "Información del Usuario";
 $pageHeader = "Detalles del Usuario";
